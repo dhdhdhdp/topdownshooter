c6cc996005cbedc05fbb0f3ecec7631afd56f364
dhdhdhdp <balycikdima@gmail.com>
2026-03-17 14:10:32 +0100

    aimen
    Player aim met muis

--- Assets/PlayerAim.cs
+++ Assets/PlayerAim.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
-public class PlayerAim
+public class PlayerAim : MonoBehaviour
 {
-    
+    private void Update()
+    {
+        /// muis in game positie omzetten
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        /// richting van speler naar muis berekenen
+        Vector2 direction = mouse - transform.position;
+
+        ///richting zetten in een hoek(rotatie)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        /// Draait de speler naar die hoek
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
 }
